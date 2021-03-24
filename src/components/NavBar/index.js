054129ef469a18c2f1ba5104e0073c7dbc695047
matheusjean <matheusjean11@hotmail.com>
2021-03-24 19:48:27 -0300

    mudando o background do navBar quando usa o scroll

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,6 +1,6 @@
-/* eslint-disable react/jsx-no-undef */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 
 import {
     Nav,
@@ -16,39 +16,55 @@ import {
 
 // eslint-disable-next-line react/prop-types
 export default function NavBar({ toogle }) {
+    const [scrollNav, setScrollNav] = useState(false);
+
+    function changeNav() {
+        if (window.scrollY >= 80) {
+            setScrollNav(true);
+        } else {
+            setScrollNav(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav);
+    }, []);
+
     return (
         <>
-            <Nav>
-                <Container>
-                    <Logo to="/">Banco Real</Logo>
-                    <MobileIcon onClick={toogle}>
-                        <FaBars />
-                    </MobileIcon>
-                    <Menu>
-                        <Item>
-                            <Links to="sobre">Sobre</Links>
-                        </Item>
-                    </Menu>
-                    <Menu>
-                        <Item>
-                            <Links to="descobrir">Descobrir</Links>
-                        </Item>
-                    </Menu>
-                    <Menu>
-                        <Item>
-                            <Links to="servicos">Serviços</Links>
-                        </Item>
-                    </Menu>
-                    <Menu>
-                        <Item>
-                            <Links to="Cadastro">Cadastre-se</Links>
-                        </Item>
-                    </Menu>
-                    <Button>
-                        <ButtonLink to="/signin">Login</ButtonLink>
-                    </Button>
-                </Container>
-            </Nav>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <Nav scrollNav={scrollNav}>
+                    <Container>
+                        <Logo to="/">Banco Real</Logo>
+                        <MobileIcon onClick={toogle}>
+                            <FaBars />
+                        </MobileIcon>
+                        <Menu>
+                            <Item>
+                                <Links to="sobre">Sobre</Links>
+                            </Item>
+                        </Menu>
+                        <Menu>
+                            <Item>
+                                <Links to="descobrir">Descobrir</Links>
+                            </Item>
+                        </Menu>
+                        <Menu>
+                            <Item>
+                                <Links to="servicos">Serviços</Links>
+                            </Item>
+                        </Menu>
+                        <Menu>
+                            <Item>
+                                <Links to="Cadastro">Cadastre-se</Links>
+                            </Item>
+                        </Menu>
+                        <Button>
+                            <ButtonLink to="/signin">Login</ButtonLink>
+                        </Button>
+                    </Container>
+                </Nav>
+            </IconContext.Provider>
         </>
     );
 }
--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,10 +1,9 @@
-/* eslint-disable no-unused-vars */
 import styled from 'styled-components';
 import { Link as LinkR } from 'react-router-dom';
 import { Link as LinkS } from 'react-scroll';
 
 export const Nav = styled.nav`
-    background: #000;
+    background: ${({ scrollNav }) => (scrollNav ? '#000' : 'transparent')};
     height: 80px;
     margin-top: -80px;
     display: flex;
